ba2137254b643dd6ca5ed234502be1c82a2609a4
Malindi John <malindi.wambua9789@gmail.com>
2025-02-10 13:08:20 +0300

    removed async, await from params in products components

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,6 +1,6 @@
-export default async function Product({params}: {params: {id: string}}){
+export default function Product({params}: {params: {id: string}}){
 
-    const { id } =  await params;
+    const { id } =  params;
 
     return <h1>Product: {id}</h1>
 }
