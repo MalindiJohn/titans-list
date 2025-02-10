3fc0c7257e9f0c874657d19f6d5d4a68a60b7437
Malindi John <malindi.wambua9789@gmail.com>
2025-02-10 13:00:33 +0300

    fixed syntax bugs in blog, greet componen

--- src/app/blog/first-post/page.tsx
+++ src/app/blog/first-post/page.tsx
@@ -1,10 +1,13 @@
+import { useRouter } from "next/router";
+
 export default function FirstPost() {
+
+    const router = useRouter();
+
     return (
         <div>
             <h1>First Post</h1>
-            <h2>
-                    <a href="/">Back to home</a>
-            </h2>
+            <button onClick={() => router.push('/')} className="bg-blue-500 text-white p-2 rounded-md">Go home</button>
         </div>
     );
 }
